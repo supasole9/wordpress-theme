2ebd5c3a47d9c88448a3d8660efe1b2782bbefa2
Ross Salanoa <ross.salanoa@dmail.dixie.edu>
2017-09-25 10:24:56 -0600

    adding index.php

--- index.php
+++ index.php
@@ -15,28 +15,23 @@
 
     <!-- Custom styles for this template -->
     <link href="blog.css" rel="stylesheet">
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
-
-    <div class="blog-masthead">
-      <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
-        </nav>
-      </div>
+    <div class="container">
+         <nav>
+              <div class="logo">
+                   <h2>Flat Earth</h2>
+              </div>
+              <ul>
+                   <li><a href="#">About</a></li>
+                   <li><a href="#">Why</a></li>
+                   <li><a href="#">How</a></li>
+                   <li><a href="#">Contact</a></li>
+              </ul>
+         </nav>
     </div>
+    <hr />
 
     <div class="container">
 
