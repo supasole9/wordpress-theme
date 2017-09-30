74c209f42fd423a7e5ae616095157817902fafa6
Ross Salanoa <ross.salanoa@dmail.dixie.edu>
2017-09-29 19:02:04 -0600

    added header & footer php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+     <div class="container">
+          <div class="row">
+               <div class="col4l">address</div>
+               <div class="col4l">tel</div>
+               <div class="col4l">email</div>
+          </div>
+     </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" href="style.css" media="screen">
+     <link rel="stylesheet" href="grid.css" media="screen">
+     <title>Flat Earth</title>
+</head>
+<body>
+     <header>
+          <div class="container">
+               <nav>
+                    <div class="logo">
+                         <h2>Flat Earth</h2>
+                    </div>
+                    <ul>
+                         <li><a href="#">About</a></li>
+                         <li><a href="#">Why</a></li>
+                         <li><a href="#">How</a></li>
+                         <li><a href="#">Contact</a></li>
+                    </ul>
+               </nav>
+          </div>
+     </header>
